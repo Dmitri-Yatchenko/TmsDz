8f813d3b751a587ebc46c948a44d81589ef2fde8
Dmitri <dmitri.yatchenko@gmail.com>
2021-11-05 16:07:11 +0300

    Fibonacci numbers

---
+++ src/com/tms/lesson4/fibonacciNumbers.java
@@ -0,0 +1,21 @@
+package com.tms.lesson4;
+
+import java.util.Arrays;
+
+public class fibonacciNumbers {
+    public static void main(String[] args) {
+
+        int[] mas = new int[10];
+
+        for (int i = 0; i < mas.length; i++){
+
+            if (i<2){
+                mas[i] = i;
+            }else {
+                mas[i] = mas[i-1] + mas[i-2];
+            }
+
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
