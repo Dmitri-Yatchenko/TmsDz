de09b98d749eb525576433dc21bfc85c4f2ea0d8
dmitri <dmitri.yatchenko@gmail.com>
2021-11-11 13:32:55 +0300

    Packages

---
+++ src/com/tms/lesson5/MainPackages.java
@@ -0,0 +1,12 @@
+package com.tms.lesson5;
+
+import com.tms.lesson5.p1.Cl;
+
+public class MainPackages {
+
+    public static void main(String[] args) {
+
+        Cl cl1 = new Cl();
+        com.tms.lesson5.p2.Cl cl2 = new com.tms.lesson5.p2.Cl();
+    }
+}
