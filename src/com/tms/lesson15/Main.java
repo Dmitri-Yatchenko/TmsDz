aef0b8e9b6ccba51b1a8cfff681614e1981dd354
Dmitri-Yatchenko <dmitri.yatchenko@gmail.com>
2021-12-25 13:57:53 +0300

    connekt

--- src/com/tms/lesson15/Main.java
+++ src/com/tms/lesson15/Main.java
@@ -1,13 +1,18 @@
 package com.tms.lesson15;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-//        String url = "jdbc:postgresql://localhost:5432/";
-//        String database = "test";
-//        String userName = "postgres";
-//        String password = "postgrespass";
-//        Connection connection = DriverManager.getConnection(url + database, userName, password);
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:postgresql://localhost:5432/";
+        String database = "lesson15";
+        String userName = "postgres";
+        String password = "1234";
+        Connection connection = DriverManager.getConnection(url + database, userName, password);
+        System.out.println(connection);
 
     }
 }
